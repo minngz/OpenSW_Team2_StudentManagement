b8b30ca02042ed6e5a2659d41b47c77cf72fdd47
Minji Kim <minji7272@naver.com>
2018-05-25 21:42:59 +0900

    [Mod] - Modified and relocated all comments
    I modify comments about all functions.
    Also, I add comment of program header.

--- StudentManagement/main.c
+++ StudentManagement/main.c
@@ -1,11 +1,32 @@
+/*
+*	< StudentManagement >
+*
+*	student.c : 
+*	student.h : 
+*
+*	original source - https://github.com/lylllcc/StudentManagment
+*	Released under the MIT license.
+*
+*	Program for managing student scores such as grades and assignments, as well as notices and assginments.
+*	Also, this program can read  a file to manage studnets and write the file.
+*	We use text file (location : StudentManagement\StudentManagement\sample_course.txt).
+*
+*	sample_course.txt - ReadFile function uses this file.
+*   If you want to read information from your file, you should change your file name to
+*   "sample_course.txt" and the information format can be found in file.c .
+*
+*	Version - v 1.1
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include"student.h"
 
 int main()
 {
+	//init total number of courses to 0
 	int courseCount = 0;
-
+	
+	//list of courses
 	COURSE *course;
 	
 	SystemSet();
