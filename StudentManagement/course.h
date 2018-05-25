63aefef1a7eaff094abb0e712ec2ac34f4150273
Jongwon Choi <cjw--@nate.com>
2018-05-26 04:35:29 +0900

    [Del] - Deleted duplicated structure define
    In each header file, the define of structure was duplicated. So I
    deleted define excluding menu.h because main.c include menu.h at first.

--- StudentManagement/course.h
+++ StudentManagement/course.h
@@ -2,34 +2,7 @@
 #define COURSE_H_INCLUDED
 #define ESC 27
 
-#include <stdlib.h>
-#include<stdio.h>
-#include<string.h>
-
-/*
-*	examScore : scores of midterm and final exam
-*	assignmentScore : assign five times
-*/
-typedef struct student
-{
-	char id[10];
-	char name[20];
-	double examScore[2];
-	double assignmentScore[5];
-}STUDENT;
-
-/*
-*	student : array of students in a course
-*	studentCount : the number of students in a course
-*/
-typedef struct course
-{
-	STUDENT student[100];
-	int studentCount;
-	char name[40];
-	char notice[10][50];
-	char assignment[5][50];
-}COURSE;
+#include "menu.h"
 
 void InitCourse(COURSE *course);
 void PrintCourseList(COURSE **course, int *courseCount);
--- StudentManagement/ui.h
+++ StudentManagement/ui.h
@@ -2,11 +2,6 @@
 #define UI_H_INCLUDED
 #define ESC 27
 
-#include <stdlib.h>
-#include<stdio.h>
-#include<string.h>
-
-
 void PrintCourseMenu();
 void PrintManagementMenu();
 void PrintScoreMenu();
